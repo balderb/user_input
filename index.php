2762421bf76bf5606080d30d88292fed7a256833
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2020-09-10 20:36:14 +0200

    recent changes

--- index.php
+++ index.php
@@ -7,14 +7,20 @@
 </head>
 <body>
 
+<h2>The order form</h2>
+
 <form action="index.php" method ="get">
 	Name: <input  type="text" name="name">
+    <input type="submit">
+    <br>
+    age: <input  type="number" name="age">
 	<input type="submit">
 </form>
 <br>
 
 <?php
  echo $_GET["name"]
+echo $_GET["age"]
 ?>
 
 </body>

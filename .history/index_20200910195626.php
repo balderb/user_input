6c4daaf8bf5e44d84e6096d6465bf0280861ac0f
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2020-09-10 20:22:09 +0200

    adding form and get method php

---
+++ .history/index_20200910195626.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<form action="site.php" method = "get">
+	Name: <input  type= "text" name="username">
+	<input type="submit">
+</form>
+<br>
+
+<?php
+
+Your name is echo $_GET["username"]
